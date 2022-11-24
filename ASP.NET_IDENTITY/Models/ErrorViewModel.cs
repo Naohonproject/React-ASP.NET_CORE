e48197cc335ffff5142f1b15bdccaa0a8cb20a93
letuanbao <letuanbao@mbg8.onmicrosoft.com>
2022-11-24 23:18:39 +0700

    add identity to api

---
+++ ASP.NET_IDENTITY/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ASP.NET_IDENTITY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+    
